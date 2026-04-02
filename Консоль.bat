35fe85bf7ebbd7ae54e75f2ab4235d5977577b9b
SigmaBudanoff <lemeshcoa@gmail.com>
2026-04-02 21:52:00 +0300

    Update Консоль.bat 1.2

--- Консоль.bat
+++ Консоль.bat
@@ -2,15 +2,23 @@
 chcp 65001 > nul
 cd /d "%~dp0"
 
-echo [1/2] Очищення старих файлів...
+echo [1/3] Очищення старих збірок...
+if exist "Artemis_OS.exe" del /f /q "Artemis_OS.exe"
 rd /s /q build dist 2>nul
 
-echo [2/2] Створення EXE (Artemis OS)...
-:: Ми просто додаємо всю папку assets цілком
-py -m PyInstaller --noconsole --onefile --add-data "assets;assets" Artemis_OS.py
+echo [2/3] Створення EXE (зачекайте)...
+:: Збираємо ОДНИМ файлом, але НЕ вшиваємо assets всередину (так надійніше)
+py -m PyInstaller --noconsole --onefile Artemis_OS.py
 
-echo.
-echo ПЕРЕМОГА! Файл Artemis_OS.exe створено в папці dist.
-echo Не забудьте, що папка 'assets' має бути поруч із ним!
+echo [3/3] Перенесення файлу та прибирання...
+if exist "dist\Artemis_OS.exe" (
+    move /y "dist\Artemis_OS.exe" .
+    rd /s /q build dist 2>nul
+    del /f /q *.spec 2>nul
+    echo СИСТЕМА ГОТОВА! Запускайте Artemis_OS.exe поруч із папкою assets.
+) else (
+    echo ПОМИЛКА: EXE не було створено. Перевірте помилки в консолі.
+)
+
 pause
 exit
