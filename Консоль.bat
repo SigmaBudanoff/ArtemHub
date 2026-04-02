8ef32d514ab0342736f72736862124fc355d0dc3
SigmaBudanoff <lemeshcoa@gmail.com>
2026-04-02 21:23:30 +0300

    Update Консоль.bat

--- Консоль.bat
+++ Консоль.bat
@@ -1,10 +1,16 @@
 @echo off
 chcp 65001 > nul
 cd /d "%~dp0"
-echo [1/3] Очищення...
+
+echo [1/2] Очищення старих файлів...
 rd /s /q build dist 2>nul
-echo [2/3] Збірка НОВОГО файлу в папку dist...
-py -m PyInstaller --noconsole --onefile -n Artemis_NEW --add-data "alarm.wav;." --add-data "clock_icon.png;." --add-data "translator_icon.png;." --add-data "qr_icon.png;." --add-data "weather_icon.png;." --add-data "calc_icon.png;." --add-data "report_icon.png;." --add-data "cosmos_icon.png;." --add-data "paint_icon.png;." Artemis_OS.py
-echo [3/3] Готово!
-del /f /q *.spec 2>nul
+
+echo [2/2] Створення EXE (Artemis OS)...
+:: Ми просто додаємо всю папку assets цілком
+py -m PyInstaller --noconsole --onefile --add-data "assets;assets" Artemis_OS.py
+
+echo.
+echo ПЕРЕМОГА! Файл Artemis_OS.exe створено в папці dist.
+echo Не забудьте, що папка 'assets' має бути поруч із ним!
+pause
 exit
